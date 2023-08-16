21466a33b8ae5c36a124d6abba55bac75b79175e
Pavel Agurov <agurov.pavel@gmail.com>
2023-08-16 13:27:53 +0200

    update recommendation_json

--- recommendation_json.py
+++ recommendation_json.py
@@ -6,11 +6,17 @@
 from recommendation import RecommendationItem
 
 recommendation_json = [
-    RecommendationItem({"F_CONTAINERIZED":1}, "Please read How to use Docker.docx"),
-    RecommendationItem({"F_REST_API":1}, "See how to use RestAPI.pdf"),
-    RecommendationItem({"F_REST_API":1, "F_API_MONEY":1}, "Monitization with RestAPI.pdf"),
-    RecommendationItem({"F_REL_DB":1}, "Important to follow instruction from Security of databases"),
-    RecommendationItem({"F_CACHE":1}, "Cache recommendation here"),
-    RecommendationItem({"F_CLOUD_PROVIDER": "AZURE"}, "Read about Azure")
+    RecommendationItem({"F_NEW_OR_MIGRATE": 1}, "Consider Azure's migration services for a seamless transition."),
+    RecommendationItem({"F_CLOUD_PROVIDER": "Azure", "F_COMPUTE" : 1},	"Consider Azure VMs or Azure Kubernetes Service for compute needs."),
+    RecommendationItem({"F_SERVERLESS_COMPUTE":1, "F_CLOUD_PROVIDER": "Azure"},	"Consider using Azure Functions for serverless compute capabilities."),
+    RecommendationItem({"F_CONTAINERIZATION": 1},	"Containerization template. Consider Azure Kubernetes Service for orchestration."),
+    RecommendationItem({"F_DOCKER": 1},	"Deploy using Azure Container Instances or Azure Kubernetes Service with Docker."),
+    RecommendationItem({"F_DATABASE": 1, "F_RELATIONAL_DB" : 1},	"Explore Azure SQL Database or Azure Database for PostgreSQL."),
+    RecommendationItem({"F_DATABASE": 1, "F_NOSQL_SERVICE" :1},	"Explore Azure Cosmos DB for NoSQL requirements."),
+    RecommendationItem({"F_CACHING": 1},	"Consider using Azure Redis Cache for enhanced performance."),
+    RecommendationItem({"F_STORAGE": 1},	"Explore Azure Blob Storage or Azure File Storage for data storage needs."),
+    RecommendationItem({"F_MIGRATE_FROM": "GCP" , "F_CLOUD_PROVIDER": "Azure"},	"Consider strategies for transitioning from BigQuery to Azure Data Lake."),
+    RecommendationItem({"F_MIGRATE_FROM": "TIBCO", "F_CLOUD_PROVIDER" : "Azure"},	"Leverage Azure's integration services for a smooth transition from TIBCO."),
+    RecommendationItem({"F_MIGRATE_FROM": "ATOS", "F_CLOUD_PROVIDER" :"Azure"},	"Consider Azure DevOps and other Azure services for digital transformation.")
 ]
 
